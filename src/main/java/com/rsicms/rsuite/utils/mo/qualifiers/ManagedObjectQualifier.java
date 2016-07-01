765e85a2f4e91aaad0181c93374a631f29fe46c3
Brent Hartwig <brent_d_hartwig@hotmail.com>
2016-07-01 06:56:07 -0400

    Presently referenced by rsuite-container-utils-lib.

---
+++ src/main/java/com/rsicms/rsuite/utils/mo/qualifiers/ManagedObjectQualifier.java
@@ -0,0 +1,21 @@
+package com.rsicms.rsuite.utils.mo.qualifiers;
+
+import com.reallysi.rsuite.api.ManagedObject;
+import com.reallysi.rsuite.api.RSuiteException;
+
+/**
+ * Interface to qualify manage objects for an arbitrary operation.
+ */
+public interface ManagedObjectQualifier {
+
+	/**
+	 * Determine if the provided MO meets criteria defined by the qualifier
+	 * class.
+	 * 
+	 * @param mo
+	 * @return True if the MO is accepted by the qualifier.
+	 * @throws RSuiteException
+	 */
+	public boolean accept(ManagedObject mo) throws RSuiteException;
+
+}
---
+++ src/main/java/com/rsicms/rsuite/utils/mo/qualifiers/QNameManagedObjectQualifier.java
@@ -0,0 +1,25 @@
+package com.rsicms.rsuite.utils.mo.qualifiers;
+
+import javax.xml.namespace.QName;
+
+import com.reallysi.rsuite.api.ManagedObject;
+import com.reallysi.rsuite.api.RSuiteException;
+import com.rsicms.rsuite.utils.mo.MOUtils;
+
+/**
+ * An managed object qualifier that accepts XML MOs with a specified QName.
+ */
+public class QNameManagedObjectQualifier implements ManagedObjectQualifier {
+
+	private QName qname;
+
+	public QNameManagedObjectQualifier(QName qname) {
+		this.qname = qname;
+	}
+
+	@Override
+	public boolean accept(ManagedObject mo) throws RSuiteException {
+		return MOUtils.hasMatchingQName(mo, qname);
+	}
+
+}
